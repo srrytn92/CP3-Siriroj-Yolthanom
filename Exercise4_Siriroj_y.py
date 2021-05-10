8451a658ad34f2300decd5318b206048d07b9288
srrytn92 <83905993+srrytn92@users.noreply.github.com>
2021-05-10 17:44:24 +0700

    Add files via upload

---
+++ Exercise4_Siriroj_y.py
@@ -0,0 +1,9 @@
+FE = 80.0
+GB = 85.5
+ICS = 81.2
+CP = 95.5
+print("---YOUR SCORE---")
+print("Foundation English :",FE)
+print("General Business",GB)
+print("Introduction to Computer System :",ICS)
+print("Computer Programming :",CP)
