52486bdc0f552a6b92d5443b9b8482875c0b6404
srrytn92 <83905993+srrytn92@users.noreply.github.com>
2021-05-19 16:12:36 +0700

    Add files via upload

---
+++ assignments/Lecture_54_siriroj_y.py
@@ -0,0 +1,34 @@
+def login():
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+    if usernameInput == "admin" and passwordInput == "1234":
+        return showMenu()
+    else:
+        return login()
+
+def showMenu():
+    print("----- iShop -----")
+    print("1. Vat Calculator")
+    print("2. Price Calculator")
+
+def menuSelect():
+    userSelected = int(input(">>"))
+    if userSelected == 1 :
+        price = int(input("Price : "))
+        print(vatCalculator(price))
+    elif userSelected == 2 :
+        print(priceCalculator())
+
+def vatCalculator(totalPrice):
+    vat = 7
+    result = totalPrice + (totalPrice * vat / 100)
+    return result
+
+def priceCalculator():
+    price1 = int(input("First Product Price : "))
+    price2 = int(input("Second Product Price : "))
+    return vatCalculator(price1 + price2)
+
+login()
+showMenu()
+menuSelect()
